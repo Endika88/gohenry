aa2b4e21ea5b58904340d393334af7d9859ad0c9
Larrea, Endika (SanTech) <endika.larrea@santander.co.uk>
2018-08-12 20:46:58 +0100

    getParent_returnParentDetails()  - repository test

--- src/main/java/org/gohenry/family/repo/GoHenryRepository.java
+++ src/main/java/org/gohenry/family/repo/GoHenryRepository.java
@@ -2,9 +2,7 @@ package org.gohenry.family.repo;
 
 import org.gohenry.family.entities.Parent;
 
-/**
- * Created by c0234499 on 12/08/2018.
- */
+
 public class GoHenryRepository {
 
     public Parent findById(int i) {
---
+++ src/test/java/org/gohenry/family/repo/GoHenryRepositoryTest.java
@@ -0,0 +1,33 @@
+package org.gohenry.family.repo;
+
+import org.gohenry.family.entities.Parent;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class GoHenryRepositoryTest {
+
+    @Autowired
+    private GoHenryRepository repository;
+
+    @Autowired
+    public TestEntityManager testEntityManager;
+
+
+
+    @Test
+    public void getParent_returnParentDetails() throws Exception {
+        Parent saveParent = testEntityManager.persistAndFlush(new Parent("go","henry",32));
+        Parent parent = repository.findById(1);
+        assertThat(parent.getName()).isEqualTo(saveParent.getName() );
+
+    }
+
+}
